acf85ad1fafc8f5a73309b1d17e0455aae4a2211
Taylor Buley <taylor.buley@gmail.com>
2012-12-04 09:25:35 -0500

    ..

--- numpy_moving_average.py
+++ numpy_moving_average.py
@@ -5,6 +5,7 @@ import numpy
 
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BCM)
+#The other option:
 #GPIO.setmode(GPIO.BOARD)
 SPICLK = 18
 SPIMISO = 23
@@ -16,8 +17,8 @@ GPIO.setup(SPIMISO, GPIO.IN)
 GPIO.setup(SPICLK, GPIO.OUT)
 GPIO.setup(SPICS, GPIO.OUT)
 
-
-# read SPI data from MCP3008 chip, 8 possible adc's (0 thru 7)
+# via http://learn.adafruit.com/reading-a-analog-in-and-controlling-audio-volume-with-the-raspberry-pi/script
+# read SPI data from MCP23008 chip, 8 possible adc's (0 thru 7)
 def readadc(adcnum, clockpin, mosipin, misopin, cspin):
         if ((adcnum > 7) or (adcnum < 0)):
                 return -1
@@ -28,7 +29,7 @@ def readadc(adcnum, clockpin, mosipin, misopin, cspin):
 
         commandout = adcnum
         commandout |= 0x18  # start bit + single-ended bit
-        commandout <<= 3    # we only need to send 5 bits here
+        commandout <<= dfs    # we only need to send 5 bits here
         for i in range(5):
                 if (commandout & 0x80):
                         GPIO.output(mosipin, True)
@@ -52,40 +53,43 @@ def readadc(adcnum, clockpin, mosipin, misopin, cspin):
         adcout /= 2       # first bit is 'null' so drop it
         return adcout
 
+dfs = 3
+debug = False
 results = numpy.array([])
 while(True):
 	# input from pin_in
 	#in = GPIO.input(SPIMISO)
         x = readadc(6, SPICLK, SPIMOSI, SPIMISO, SPICS)
+	#if False == x:
+	#	continue
 	#moving average
-	w = numpy.ones( x, 'd')
+	#w = numpy.ones( x, 'd')
 	#other windows
 	s = numpy.array( x )
 	#hanning
 	#w = numpy.hanning(s)
 	#hamming
-	#w = numpy.hamming(s)
+	w = numpy.hamming(s)
 	#bartlett
 	#w = numpy.bartlett(s)
 	#blackman
 	#w = numpy.blackman(s)
 	#kaiser
 	#w = numpy.kaiser(s, beta = 14)
-
-	print('current', x, 'running', results.mean(), results.std() )
-	if x > ( results.mean() - ( 3 * results.std() ) ) and x < ( results.mean() + ( 3 * results.std() ) ):
-		print("OK", x)
-	elif x > ( results.mean() + ( 3 * results.std() ) ):
-		print("TOO BIG", results.mean() + ( 3 *  results.std() ) )
-	elif x < ( results.mean() + ( 3 * results.std() ) ):
-		print("TOO SMALL", results.mean() + ( 3 *  results.std() ) )
+	if True == debug:
+		print('current', x, 'running', results.mean(), results.std() )
+	#if x > ( results.mean() - ( dfs * results.std() ) ) and x < ( results.mean() + ( dfs * results.std() ) ):
+	#	print("OK", x)
+	if x > ( results.mean() + ( dfs * results.std() ) ):
+		print("TOO BIG", results.mean() + ( dfs *  results.std() ) )
+	elif x < ( results.mean() - ( dfs * results.std() ) ):
+		print("TOO SMALL", results.mean() + ( dfs *  results.std() ) )
 	y = numpy.convolve(w/w.sum(),x,mode='valid')
 	results = numpy.append( results, y.sum() )
-
 
 	#print(s);
 	#if 0 != s:
 	#	pprint.pprint("Movement: " + str(s))
-	time.sleep(1)
+	time.sleep(1/10)
 
 
